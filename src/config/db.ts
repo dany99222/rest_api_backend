f1cca4bcdb481292e38f0bc0baeb8768f3249bac
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-06 13:01:35 -0800

    fix: DATABASE_URL and SSL config

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URl!, {
+const db = new Sequelize(process.env.DATABASE_URL!, {
     models: [__dirname + '/../models/**/*'],
     logging: false
 });
